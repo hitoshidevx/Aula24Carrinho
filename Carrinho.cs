5e79add4b7a84829a0d4d56bf4b0240686dc76e8
unknown <g.hitoshi.yokogawa@gmail.com>
2020-06-24 10:10:15 -0300

    first commit

---
+++ Carrinho.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using System;
+
+namespace Carrinho
+{
+    public class Carrinho
+    {
+        
+        //Criei a variável para o valor total do produto.
+        public float ValorTotal { get; set; }
+
+        //Criei uma lista de produtos para o carrinho.
+        List<Produto> carrinho = new List<Produto>();
+
+        //Variável para a adição do produto.
+        public void Adicionar(Produto produto)
+        {
+            carrinho.Add(produto);
+        }
+
+        //Variável para a exclusão de um ou mais produtos.
+        public void Excluir(Produto produto)
+        {
+            carrinho.Remove(produto);
+        }
+
+        //Leitura dos produtos para o usuário.
+        public void MostrarProdutos()
+        {
+            if(carrinho != null){
+                foreach(Produto p in carrinho){
+
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.WriteLine($"Nome: {p.Nome} - Preço: {p.Preco}");
+                    Console.ResetColor();
+
+                }
+            }
+        }
+
+        //Valor total dos produtos.
+        public void MostrarValorTotal()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            
+            if(carrinho != null){
+
+                foreach(Produto p in carrinho){
+
+                ValorTotal += p.Preco;
+
+                }   
+            Console.WriteLine($"O valor total é igual a: {ValorTotal}");
+            Console.ResetColor();
+
+            }else{
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("O seu carrinho está vazio!");
+                
+            }
+        }
+
+        //Update em um ou mais produtos.
+        public void AlterarProduto(int _codigo, Produto _novoproduto)
+        {
+            carrinho.Find(x => x.Codigo == _codigo).Nome = _novoproduto.Nome;
+            carrinho.Find(x => x.Codigo == _codigo).Preco = _novoproduto.Preco;
+        }
+
+
+
+    }
+}
---
+++ Produto.cs
@@ -0,0 +1,22 @@
+namespace Carrinho
+{
+    public class Produto
+    {
+        
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        public Produto(){
+
+        }
+        //Produto - Video-Game.
+        public Produto(int _codigo, string _nome, float _preco){
+
+            this.Codigo = _codigo;
+            this.Nome = _nome;
+            this.Preco = _preco;
+
+        }
+    }
+}
